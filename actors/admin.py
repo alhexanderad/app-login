ff7c434ff7f8ce4abbb4645d39331384e6c6946e
Alex Hector Astuhuaman Delgado <alhexanderad@gmail.com>
2022-03-30 00:16:53 -0500

    Se realizo la parte de proxy mode, para poder filtar las peliculas por medio de films y commercials

---
+++ actors/admin.py
@@ -0,0 +1,5 @@
+from re import A
+from django.contrib import admin
+from actors.models import Actor
+
+admin.site.register(Actor)
--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+admin.site.site_header = 'Movies Administration'
+admin.site.index_title = 'Manage the movie site'
+
 if settings.DEBUG:
-    
   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
